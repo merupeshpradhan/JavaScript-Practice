bb0a393a5385326494853de7a020db0c9d768e81
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-07-13 23:50:29 +0530

    Add function to check if age is between 10 and 20

---
+++ 13th_Expressions_&_Condilionals_Practice.js
@@ -0,0 +1,10 @@
+// 1) Use code Operator to find whether the age of a person lies betwen 10 and 20?
+function matchingUserFound(age) {
+  if (age >= 10 && age <= 20) {
+    console.log("Age is lies between 10 and 20");
+  } else {
+    console.log("Age did not lies between 10 and 20");
+  }
+}
+
+matchingUserFound(25);
