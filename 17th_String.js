20cfdbb5463353cc2bc5b7ef5eece205eedeba3f
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-07-16 12:37:50 +0530

    Add 17th_String.js with examples of string manipulation and output formatting

---
+++ 17th_String.js
@@ -0,0 +1,15 @@
+console.log("\nSingle Quets:- ");
+console.log("Rupesh\n");
+
+console.log("Double Quets:- ");
+console.log("Rupesh\n");
+
+let name = "Rupesh Pradhan";
+console.log("Name is", name);
+console.log("Name length is: ", name.length);
+console.log(`using Interpoulation geting data: ${name}\n`);
+
+console.log("The New line use :- \n Rupesh");
+console.log("The Tale use :- \t Rupesh");
+console.log("The Carriage return:- \n Helloo \r Rupesh");
+
