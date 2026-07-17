9805a835bb9a40819ec1e7286bc86496f963aaec
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-07-17 14:11:43 +0530

    Add 20th_Arrays.js with examples of array data manipulation, including length check and value update

---
+++ 20th_Arrays.js
@@ -0,0 +1,17 @@
+let names = ["Rupesh", "Manas", "Adii", "Amiya"];
+
+// Print all array data
+console.log("\nAll data of array:", names, "\n");
+
+// Check length of array
+console.log("Length of array:", names.length, "\n");
+
+// Print Only single array data
+console.log("Single data of array:", names[0], "\n");
+
+// New changing data aray
+console.log("Old array data:", names);
+// chaning the value
+names[1] = "Rashmi";
+console.log("Now one string change and this is the print:", names, "\n");
+
