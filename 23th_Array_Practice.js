43a35cb31719c9fcb923aca2653c71aeedc3f983
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-07-22 18:53:49 +0530

    Refactor 23th_Array_Practice.js to comment out previous examples and add a factorial calculation using reduce; update tempCodeRunnerFile.js to remove unused code.

--- 23th_Array_Practice.js
+++ 23th_Array_Practice.js
@@ -1,18 +1,40 @@
 // Q1) Create an array of number and take input from the user to add number to this array.
-let numbers = [20, 4.5, 6, 83];
-console.log("User without adding, Arry numbers are:", numbers);
+// let numbers = [1, 5, 6, 3];
+// console.log("User without adding, Arry numbers are:", numbers);
 // let userInput = Number(prompt("Please Enter a number"));
 // numbers.push(userInput);
-console.log("After user adding number:", numbers);
+// console.log("After user adding number:", numbers);
 
 // Q2) Keep adding numbers to the array in (1) until 0 is add to the array.
+// let numbers = [20, 45, 6, 83, 55, 2, 88];
+// let input;
+// do {
+//   input = Number(prompt("Provide a number to add in array"));
+//   numbers.push(input);
+//   console.log(numbers);
+// } while (input !== 0);
 
 // 3) filter for numbers devisible by 10 from a given array
-function devisible(number) {
-  if (number % 10 === 0) {
-    return number;
-  }
-}
-let value = numbers.filter(devisible);
+// function devisible(number) {
+//   if (number % 10 === 0) {
+//     return number;
+//   }
+// }
+// let value = numbers.filter(devisible);
 
-console.log(value);
+// console.log(value);
+
+// 4) Create an array of square of given array numbers
+// console.log("\nOrginal Array:", numbers);
+// let finaleValue = numbers.map((n) => {
+//   return n * n;
+// });
+// console.log("Square of array is:", finaleValue);
+
+// 5) Use Reduce to calculate factorial of a given number from an array of first n natural numbers (n being the number whose factorial needs to be calcualated)
+let numbers = [1,2,3,4]
+console.log("\nOrginal array:",numbers);
+let calculateNumber = numbers.reduce((n1, n2) => {
+  return n1 * n2;
+});
+console.log("After use reduce():",calculateNumber);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,1 @@
-let a1 = [2, 3, 4, 9, 21];
-let a2 = a1.splice(0,6,22,23)
-console.log(a2);
+reduce
