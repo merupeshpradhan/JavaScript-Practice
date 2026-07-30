a15cc089ded61437da17ea6be222130623f67c36
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-07-31 00:29:54 +0530

    Fix typo in HTML and update JavaScript for alert, prompt, and confirm functionality

--- 25th_alert_prompt_conform.js
+++ 25th_alert_prompt_conform.js
@@ -1,3 +1,11 @@
 alert("Welcome to alert(), prompt() & conform() Practice.");
-let names = prompt("Your fraiend names are...");
-document.write(names);
+// let names = prompt("Your fraiend names are...");
+let names = prompt("Your fraiend names are...", "Amiya, Adii, Manas, etc");
+
+let desplyNames = confirm("Are you want to display all names");
+
+if (desplyNames) {
+  document.write(names);
+} else {
+  document.write("For display the name please slect the Yes");
+}
