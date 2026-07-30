45a375d6c524ee85692adf155e108d97346ff652
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-07-30 23:55:53 +0530

    Fix typo in HTML and add JavaScript for alert, prompt, and document.write functionality

--- 25th_alert_prompt_conform.js
+++ 25th_alert_prompt_conform.js
@@ -1,1 +1,3 @@
-alert("Welcome to alert(), prompt() & conform() Practice.")
+alert("Welcome to alert(), prompt() & conform() Practice.");
+let names = prompt("Your fraiend names are...");
+document.write(names);
