6db7c4bd74ce8fb3ddca0c0944782b1b6b899209
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-08-02 12:48:13 +0530

    Comment out unused script references in 25th_alert_prompt_conform.html and add 25th_javaScript_in_the_browser_practice.js for user input examples.

---
+++ 25th_javaScript_in_the_browser_practice.js
@@ -0,0 +1,33 @@
+/* Q1) Write a program using prompt function to take input of age as a value from the user and use alert to tell him if he can drive! */
+// let age = Number(prompt("Give your age"));
+// if (age >= 18) {
+//   alert("You can drive!");
+// } else {
+//   alert("You cannot drive!");
+// }
+
+/* Q2) In Q1 use confirm to ask the user if he wants to see the prompt again */
+// let condition = true;
+// while (condition) {
+//   let age = Number(prompt("Give your age"));
+//   alert("You can drive!");
+//   condition = confirm("User if you want to see the prompt again");
+// }
+
+/* Q3) In the previous question, use console.error to log the error if the age entered is negative */
+// let age = Number(prompt("Give your age"));
+// if (age < 0) {
+//   console.error("Provide correct age.");
+// }
+
+/* Q4) Write a program to change the url to google.com (Redirection) if user enter a number is greater than 4 */
+// let number = Number(prompt("Provide a Number"));
+// if (number >= 4) {
+//   location.href = "https://www.google.com";
+// } else {
+//   alert("Please provide crrect number");
+// }
+
+/* Q5) Change the background of the page to yellow, red or any other color based on user input through prompt */
+// let colorName = prompt("Provide a color name");
+// document.body.style.background = colorName;
