efbf5919dc211cdf93b8b0b219794a13da39cc94
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-08-05 23:37:33 +0530

    Add initial files for Snake, Water, and Gun game including HTML, CSS, and JavaScript

---
+++ Projects/24_Exercise2_Snake_Water_Gun_Game/index.js
@@ -0,0 +1,12 @@
+/* Use javaScript to create a game of snake, water & Gun. The game should ask you to enter S, W and G. The computer should be value to randomly generate S, W and G and declare Win or Loss using alert Use confirm and prompt where required {Here use Math.random() for getting nambur numbers} */
+
+let winer = document.getElementById("winner");
+let btn = document.getElementById("btn");
+
+let choices = ["S", "W", "G"];
+
+let randomSelect;
+
+btn.onclick = function () {
+  
+};
