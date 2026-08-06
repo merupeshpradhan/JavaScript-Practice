e168152bd88643e840acdcd2537c1f73695ad1e4
Rupesh Pradhan <merupeshpradhan@gmail.com>
2026-08-06 11:18:22 +0530

    Refactor game logic to improve winner declaration and fix variable name typo for 'winner'

--- Projects/24_Exercise2_Snake_Water_Gun_Game/index.js
+++ Projects/24_Exercise2_Snake_Water_Gun_Game/index.js
@@ -10,53 +10,48 @@ let randomSelect;
 
 btn.onclick = function () {
   let userChoice = prompt("Enter S for Snake, W for Water and G for Gun");
+  userChoice.toUpperCase();
+  
   randomSelect = Math.floor(Math.random() * 3 + 1);
   console.log(choices[randomSelect]);
 
-  while (true) {
-    if (choices[randomSelect] === "S" && userChoice === "s") {
-      winnner.textContent = "It's a Draw!";
-      break;
-    }
+  if (choices[randomSelect] === "S" && userChoice === "s") {
+    console.log("It's a Draw!");
+    winner.textContent = "It's a Draw!";
+  }
 
-    if (choices[randomSelect] === "S" && userChoice === "W") {
-      winner.textContent = "Snake";
-      break;
-    }
+  if (choices[randomSelect] === "S" && userChoice === "W") {
+    winner.textContent = "Winner is Snake";
+  }
 
-    if (choices[randomSelect] === "S" && userChoice === "G") {
-      winner.textContent = "Gun";
-      break;
-    }
+  if (choices[randomSelect] === "S" && userChoice === "G") {
+    winner.textContent = "Winner is Gun";
+  }
 
-    if ((choices[randomSelect] === "W") && userChoice === "W") {
-      winnner.textContent = "It's a Draw!";
-      break;
-    }
+  if (choices[randomSelect] === "W" && userChoice === "W") {
+    console.log("It's a Draw!");
 
-    if (choices[randomSelect] === "W" && userChoice === "G") {
-      winner.textContent = "Water";
-      break;
-    }
+    winner.textContent = "It's a Draw!";
+  }
 
-    if (choices[randomSelect] === "W" && userChoice === "S") {
-      winner.textContent = "Snake";
-      break;
-    }
+  if (choices[randomSelect] === "W" && userChoice === "G") {
+    winner.textContent = "Winner is Water";
+  }
 
-    if (choices[randomSelect] === "G" && userChoice === "G") {
-      winnner.textContent = "It's a Draw!";
-      break;
-    }
+  if (choices[randomSelect] === "W" && userChoice === "S") {
+    winner.textContent = "Winner is Snake";
+  }
 
-    if (choices[randomSelect] === "G" && userChoice === "S") {
-      winner.textContent = "Gun";
-      break;
-    }
+  if (choices[randomSelect] === "G" && userChoice === "G") {
+    console.log("It's a Draw!");
+    winner.textContent = "It's a Draw!";
+  }
 
-    if (choices[randomSelect] === "G" && userChoice === "W") {
-      winner.textContent = "Water";
-      break;
-    }
+  if (choices[randomSelect] === "G" && userChoice === "S") {
+    winner.textContent = "Winner is Gun";
+  }
+
+  if (choices[randomSelect] === "G" && userChoice === "W") {
+    winner.textContent = "Winner is Water";
   }
 };
